6fd7b741111a25fb6caa2d51385bbf5af80c62ed
DESKTOP-5H2GF3M\kyawa <y980055617@gmail.com>
2025-06-12 20:58:43 +0900

    add(file): new depth method

--- run_inference.sh
+++ run_inference.sh
@@ -1,23 +1,24 @@
-# CUDA_VISIBLE_DEVICES=2 python3 depth_splatting_inference.py \
-#     --input_video_path ./source_video/124.mp4 \
-#     --output_video_path ./outputs/124_splatting_results.mp4 \
+#!/bin/bash
+
+ORIGIN="inpainting_inference.py"
+MEMORY="inpainting_inference_memory_fix.py"
+REPLACE="inpainting_inference_with_replace.py"
+
+CUDA_NUM=0
+
+# CUDA_VISIBLE_DEVICES=$CUDA_NUM python3 depth_splatting_inference.py \
+#     --input_video_path ./source_video/142.mp4 \
+#     --output_video_path ./outputs/142_splatting_results.mp4 \
 #     --unet_path ./weights/DepthCrafter \
 #     --pre_trained_path ./weights/stable-video-diffusion-img2vid-xt-1-1
 
-# python3 inpainting_inference_with_replace.py \
-#     --input_video_path ./outputs/5_splatting_results.mp4 \
-#     --save_dir ./outputs \
-#     --unet_path ./weights/StereoCrafter \
-#     --pre_trained_path ./weights/stable-video-diffusion-img2vid-xt-1-1 \
-#     --tile_num 2 \
-#     --num_inference_steps 1 \
-#     --frames_chunk 100 \
+TARGET=$MEMORY
 
-CUDA_VISIBLE_DEVICES=1 python3 inpainting_inference.py \
-    --input_video_path ./outputs/124_splatting_results.mp4 \
+CUDA_VISIBLE_DEVICES=$CUDA_NUM python3 $TARGET \
+    --input_video_path ./outputs/141_splatting_results.mp4 \
     --save_dir ./outputs \
     --unet_path ./weights/StereoCrafter \
     --pre_trained_path ./weights/stable-video-diffusion-img2vid-xt-1-1 \
     --tile_num 2 \
-    --num_inference_steps 8 \
-    --frames_chunk 100 \
+    --num_inference_steps 1 \
+    --frames_chunk 100
